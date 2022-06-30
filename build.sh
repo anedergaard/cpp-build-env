56dbb9f782aecdade1560c5131be92a9ffc34700
anedergaard <ape.anp@gmail.com>
2022-06-30 15:44:15 +0200

    bumped container base image to 22.10 bumped clang to 14 bumped cmake to 3.23.2
    enter container by ./build.sh -s

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ print_help()
 {
     echo "***************** Usage ******************"
     echo "-e build docker environment"
-    echo "-d run images as a daemon"
-    echo "--load shows the htop for the running container"
+    echo "-s start shell in image"
     echo "-h this help text"
 }
 
@@ -16,29 +15,27 @@ fi
 
 case $1 in
     -e)
-	docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t distcc/ccache -f Dockerfile .
+	docker build -t cpp-build-env -f Dockerfile .
 	;;
-    -d)
-	docker run -p 3632:3632 -p 3633:3633 -d --name localdistcc --network host distcc/ccache
-	#export LOCAL_DISTCC_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' localdistcc)
-	#export LOCAL_DISTCC_NPROC=$(docker exec -it localdistcc nproc)
-	#export DISTCC_HOSTS="$LOCAL_DISTCC_IP/$LOCAL_DISTCC_NPROC localhost"
-	;;
-    #--update)
-	#export LOCAL_DISTCC_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' localdistcc)
-	#export LOCAL_DISTCC_NPROC=$(docker exec -it localdistcc nproc)
-	#export DISTCC_HOSTS="$LOCAL_DISTCC_IP/$LOCAL_DISTCC_NPROC localhost"
-	#echo $LOCAL_DISTCC_IP
-	#;;
-    #-r)
-	#docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 clion/remote-ubuntu:20.04
-	#;;
-    --load)
-	docker exec -it localdistcc htop
+    -s)
+      DOCKER_ARGS="--privileged \
+      --rm=true \
+      -it \
+      -u user \
+      -v $HOME:/home/user \
+      -v empty:/home/user/.local/bin \
+      -v $(pwd):/tmp/$(basename "$(pwd)") \
+      -w /tmp/$(basename "$(pwd)")"
+
+	    # Just run the command if we are inside a docker container
+      if [ -f /.dockerenv ]; then
+          $*
+      else
+          DOCKER_COMMAND="docker run $DOCKER_ARGS --network host cpp-build-env"
+          $DOCKER_COMMAND
+      fi
 	;;
     -h)
 	print_help
 ;;
 esac
-
-
